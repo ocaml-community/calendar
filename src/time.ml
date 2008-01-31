06215e4b3c61057f8dc2dcbe831f370027c8e659
signoles <signoles@5c1b4d90-02b3-475d-951b-de63f8a2b13d>
2008-01-31 09:02:33 +0000

    headers
    git-svn-id: svn://forge.ocamlcore.org/svn/calendar/trunk@140 5c1b4d90-02b3-475d-951b-de63f8a2b13d

--- src/time.ml
+++ src/time.ml
@@ -1,19 +1,25 @@
-(*
- * Calendar library
- * Copyright (C) 2003 Julien SIGNOLES
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License version 2, as published by the Free Software Foundation.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * See the GNU Library General Public License version 2 for more details
- *)
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of Calendar.                                        *)
+(*                                                                        *)
+(*  Copyright (C) 2003-2008 Julien Signoles                               *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, either version 2.1 of the Licence, or (at your option)    *)
+(*  version 3.                                                            *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public Licence version 2.1 for more        *)
+(*  details (enclosed in the file LGPL).                                  *)
+(*                                                                        *)
+(**************************************************************************)
 
-(*i $Id: time.ml,v 1.17 2004-11-17 16:05:19 signoles Exp $ i*)
+(*i $Id: time.ml,v 1.18 2008-01-31 09:02:33 signoles Exp $ i*)
 
 (*S Introduction.
 
